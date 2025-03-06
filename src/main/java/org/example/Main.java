96b9a0e6bbf7c3a68da5e3a008a917d426e660c7
chair <chairi.afaf@gmail.com>
2025-03-06 11:20:12 +0100

    print "hello from feature2"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        System.out.println("this is feature 2");
+        System.out.println("Hello from feauture2");
     }
 
 
